b8a6d521636aa1ea2c7a60df7f1b666c3d17f193
Youzica <88140180+Youzica@users.noreply.github.com>
2023-11-23 14:28:46 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const wx = {
-    appid: 'wxdad78b793ffc1d03',
-    secret: '47fb51f35cd669d5caca1081991c68eb',
+    appid: 'APPID填写位置',
+    secret: '秘钥填写位置',
 }
 const url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID填写位置&secret=秘钥填写位置'
 
